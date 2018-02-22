e4cdcd744d4c51eea9a5eb8e3103d74b0c9b133d
Nadim Uddin <nadim.uddin1995@gmail.com>
2018-02-22 02:33:05 -0500

    learn about value and pointer reciever methods for structs

--- basicStructs.go
+++ basicStructs.go
@@ -9,13 +9,24 @@ type car struct {
 	topSpeeKmh float64
 }
 
+func (c car) totalPedal() uint16 {
+	return (c.gasPedal - c.brakePedal)
+}
+
+func (c *car) newSpeed(speed float64){
+	c.topSpeeKmh = speed
+}
+
 func main(){
 	var sampleCar car = car{gasPedal: 1000,
 						brakePedal:200,
 						steeringWheel: 12,
 						topSpeeKmh: 12345.43}
 
-	fmt.Println("SHOULD BE 1000: ", sampleCar.gasPedal)
-
+	// fmt.Println("SHOULD BE 1000: ", sampleCar.gasPedal)
+	// fmt.Println("TOTAL PEDAL: ", sampleCar.totalPedal())
+	fmt.Println("OLD SPEED: ", sampleCar.topSpeeKmh)
+	sampleCar.newSpeed(500.43)
+	fmt.Println("NEW SPEED: ", sampleCar.topSpeeKmh)
 
 }
