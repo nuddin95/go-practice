92d3f96321ebeb7468d0dc49f20f1e015ac778a9
Nadim Uddin <nadim.uddin1995@gmail.com>
2018-02-22 22:32:39 -0500

    playing around with some rss code in server.go

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http")
+	// "github.com/mmcdole/gofeed")
 
 func index_handler(w http.ResponseWriter, r *http.Request){
 	//Fprintf first arguement is what to write to in
@@ -11,9 +12,19 @@ func index_handler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Go is pretty cool")
 }
 
+// func rss(w http.ResponseWriter, r *http.Request){
+// 	fp := gofeed.NewParser()
+// 	feed, _ := fp.ParseURL("https://www.androidpolice.com/feed/")
+// 	fmt.Println(feed)
+// 	fmt.Fprintf(w, feed.Title)
+// }
+
 func main(){
 	//specifies the path and what function to run
 	http.HandleFunc("/", index_handler)
+
+	//rss feed
+	// http.HandleFunc("/rss", rss)
 
 	//creates the server
 	http.ListenAndServe(":8000", nil)
